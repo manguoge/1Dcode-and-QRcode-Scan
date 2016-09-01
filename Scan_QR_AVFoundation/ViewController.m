d63796ff14503d28270f2142b0f2786b7cec481d
comfouriertech <comfouriertech@comfouriertechdemac-mini.local>
2016-09-01 15:01:08 +0800

    Initial Commit

---
+++ Scan_QR_AVFoundation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Scan_QR_AVFoundation
+//
+//  Created by comfouriertech on 16/9/1.
+//  Copyright © 2016年 ronghua_li. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
